1226c5267bb28fe21cb00ddbe9854571dd04507a
Hunorabraham <hunor335@gmail.com>
2024-10-14 21:23:07 +0200

    words

--- script.js
+++ script.js
@@ -17,15 +17,46 @@ class maybe{
 }
 
 document.querySelectorAll("button")[0].onclick=()=>{
-	
+	let tokenList = tokenise(inText.value);
+	if(!tokenList.ok){
+		console.log(`err tokeniser: ${tokenList.value}`);
+		return;
+	}
+	console.log(tokenList.value);
 }
 
-
+let whiteSpaces = [" ", "\t", "\n"];
+//currently only single character tokens
+let nonSeparatedTokens = [".",",",";",":","(",")","[","]","{","}","!","?","*","/",'"',"'","*","+","-","=",">","<"];
 
 function tokenise(source){
 	if(!source) return new maybe(false, "source is empty, or not type string");
 	let index = 0;
+	let words = [];
+	let wordAccumulator = "";
+	
+	//first separate it into words, based on whitespaces and other operators/special characters
 	while(index < source.length){
+		let currentCharacter = source[index];
 		
+		if(whiteSpaces.includes(currentCharacter)){
+			if(wordAccumulator){
+				words.push(wordAccumulator);
+				wordAccumulator = "";
+			}
+		}
+		else if(nonSeparatedTokens.includes(currentCharacter)){
+			if(wordAccumulator){
+				words.push(wordAccumulator);
+				wordAccumulator = "";	
+			}
+			words.push(currentCharacter);
+		}
+		else{
+			wordAccumulator += currentCharacter;			
+		}
+		
+		index++;
 	}
+	return new maybe(true, words);
 }
